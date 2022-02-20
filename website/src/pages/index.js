587410434524e73620159a149a14900e901a1dd5
josephpak <josephpak2014@gmail.com>
2022-02-19 23:50:36 -0800

    Anything new

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>New Header</h1>
+    <p>Add a secondary Header.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
@@ -23,7 +23,6 @@ const IndexPage = () => (
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
 )
